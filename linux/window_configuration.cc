ec5a7f8e1975a2b49cba20fb622c10361a40a4c2
Lucas <lutamaribuchi@gmail.com>
2020-06-08 02:04:26 -0300

    initial commit

---
+++ linux/window_configuration.cc
@@ -0,0 +1,5 @@
+#include "window_configuration.h"
+
+const char *kFlutterWindowTitle = "travel_agency_front";
+const unsigned int kFlutterWindowWidth = 1280;
+const unsigned int kFlutterWindowHeight = 720;
