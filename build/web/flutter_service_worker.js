82a9219fa49676d3987f87324770a1c0d4e74d61
Lucas <lutamaribuchi@gmail.com>
2020-07-06 00:04:55 -0300

    Deploy

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "b1a8c709a3d93974bbd8534af401d134",
 "/": "b1a8c709a3d93974bbd8534af401d134",
-"main.dart.js": "8c8e78f529169897f225626bc0a4537d",
+"main.dart.js": "a7f3cff21d3700460647563fb488c049",
 "manifest.json": "75f1bf16cb220cba37f21ffa0a677b25"
 };
 
